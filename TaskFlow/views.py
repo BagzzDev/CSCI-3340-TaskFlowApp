bed50a0cb02a235ecfcac0c92efa534a1a7a77a7
Ernesto Perez <perez.ernesto@protonmail.com>
2024-11-04 22:02:46 -0600

    login implementation

--- TaskFlow/views.py
+++ TaskFlow/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 # Create your views here.
-def login_user(request):
-    return render(request, 'login.html', {})
+def home(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            #messages.success(request, "Login Successful!")
+            return redirect('home')
+        else:
+            #messages.error(request, "Login Error")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
